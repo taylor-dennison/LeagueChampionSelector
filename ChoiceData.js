e1b613c6c9c7d735ab3f22bcb3377d9009910c25
taylor-dennison <78880285+taylor-dennison@users.noreply.github.com>
2021-02-27 16:27:47 -0500

    Add files via upload

---
+++ ChoiceData.js
@@ -0,0 +1,122 @@
+var selectInfo = {
+  "choices" : [
+        // First set of options to choose
+        {
+          "key": "Main",
+          "option_1"   : "Top Lane",
+          "option_2"   : "Jungle",
+          "option_3"   : "Middle Lane",
+          "option_4"   : "Bottom Lane - ADC",
+          "option_5"   : "Bottom Lane - Support",
+          "description": "Select a lane to play in!",
+          "depth"      : "0"
+        },
+        //Second options from first choice
+        {
+          "key"        : "Top Lane",
+          "option_1"   : "Top Tank Role",
+          "option_2"   : "Fighter Role",
+          "description": "What type of Top Lane role would you like to play?",
+          "depth"      : "1"
+        },
+        {
+          "key": "Jungle",
+          "option_1": "Counter Jungling",
+          "option_2": "Ganking Focus",
+          "description": "What style of gameplay would you prefer for Jungle play?",
+          "depth"      : "1"
+        },
+        {
+          "key": "Middle Lane",
+          "option_1": "Mage Role",
+          "option_2": "Assassin Role",
+          "description": "What type of Middle Lane role would you like to play?",
+          "depth"      : "1"
+        },
+        {
+          "key": "Bottom Lane - ADC",
+          "option_1": "Auto-Attack ADC",
+          "option_2": "Castor ADC",
+          "description": "Which Attack style do you prefer to play?",
+          "depth"      : "1"
+        },
+        {
+          "key": "Bottom Lane - Support",
+          "option_1": "Tank Role",
+          "option_2": "Utility Role",
+          "description": "Which support role would you like to play??",
+          "depth"      : "1"
+        },
+        //third options after making second choice
+        {
+          "key": "Top Tank Role",
+          "option_1": "Garen",
+          "option_2": "Nasus",
+          "description": "Select a Tank Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Fighter Role",
+          "option_1": "Darius",
+          "option_2": "Yorick",
+          "description": "Select a Fighter Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Counter Jungling",
+          "option_1": "Lee Sin",
+          "option_2": "Udyr",
+          "description": "Select a Counter Jungling Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Ganking Focus",
+          "option_1": "Rammus",
+          "option_2": "Warwick",
+          "description": "Select a Ganking Focus Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Mage Role",
+          "option_1": "Annie",
+          "option_2": "Lux",
+          "description": "Select a Mage Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Assassin Role",
+          "option_1": "Zed",
+          "option_2": "Katarina",
+          "description": "Select a Assassin Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Auto-Attack ADC",
+          "option_1": "Ashe",
+          "option_2": "Caitlyn",
+          "description": "Select a Auto-Attack Focus Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Castor ADC",
+          "option_1": "Varus",
+          "option_2": "Miss Fortune",
+          "description": "Select an Ability Casting DamageChampion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Tank Role",
+          "option_1": "Nautilus",
+          "option_2": "Braum",
+          "description": "Select a Tank Support Champion",
+          "depth"      : "2"
+        },
+        {
+          "key": "Utility Role",
+          "option_1": "Thresh",
+          "option_2": "Soraka",
+          "description": "Select a Utility Support Champion",
+          "depth"      : "2"
+        }
+      ]
+}
